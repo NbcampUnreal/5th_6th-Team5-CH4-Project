0a8ecbfcaf79c20a48840a478735cef17bafbc48
lived000 <dvse2012@gmail.com>
2025-12-18 11:08:34 +0900

    Revert "??"
    This reverts commit 7bcb1b211492a82b50d699bd515e171b1de4ce04.

--- Source/Team5_Project/PlayerCharacter/PlayerCharacter.h
+++ Source/Team5_Project/PlayerCharacter/PlayerCharacter.h
@@ -9,9 +9,6 @@
 #include "InputActionValue.h"
 #include "PlayerCharacter.generated.h"
 
-// [추가] 컴포넌트 클래스 전방 선언 (헤더 참조 충돌 방지)
-class UTeam5_DamageTakenComponent;
-
 UCLASS()
 class TEAM5_PROJECT_API APlayerCharacter : public ACharacter
 {
@@ -32,20 +29,12 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	// [추가] 서버로 공격 요청을 보내는 RPC 함수 (클라이언트 -> 서버)
-	UFUNCTION(Server, Reliable, WithValidation)
-	void Server_Attack(FVector Start, FVector Dir);
-	
 protected:
 	UPROPERTY(EditAnywhere)
 	class USpringArmComponent* SpringArm;
 
 	UPROPERTY(EditAnywhere)
 	class UCameraComponent* Camera;
-
-	// [추가] 내 몸에 부착된 데미지 처리 컴포넌트를 저장할 변수
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
-	UTeam5_DamageTakenComponent* DamageComp;
 
 private:
 	UPROPERTY(VisibleAnywhere, Category = "Input")
@@ -68,13 +57,6 @@ private:
 
 	UPROPERTY(BlueprintReadOnly, Category = "Animation", meta = (AllowPrivateAccess = "true"))
 	bool bIsFalling = false;
-
-	// [수정] 트레이스 함수: 서버에서도 궤적을 계산해야 하므로 시작점(Start)과 방향(Dir)을 인자로 받도록 변경
-	bool DoLineTrace(FHitResult& OutHit, FVector Start, FVector Dir) const;
-    
-	// [추가] 컴포넌트가 "죽었다"고 신호를 보내면 실행될 함수
-	UFUNCTION()
-	void OnDeath();
 
 	UFUNCTION()
 	void Attack(const struct FInputActionValue& Value);
