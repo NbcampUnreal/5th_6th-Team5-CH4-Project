d22e51387a1cfa1abfabc17a935fe3b227448d7d
JJ00ng <whdgur0316@Naver.com>
2025-12-22 12:54:11 +0900

    add // c++ 게임모드

---
+++ Source/Team5_Project/UI/T5UIGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "T5UIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM5_PROJECT_API AT5UIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
