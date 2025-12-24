1017ddbe6175b69d1a5a16329f3297eb598f49d4
lived000 <dvse2012@gmail.com>
2025-12-24 20:25:08 +0900

    되라

--- Source/Team5_Project/UI/T5UIGameModeBase.h
+++ Source/Team5_Project/UI/T5UIGameModeBase.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Source/Team5_Project/UI/T5UIGameModeBase.h
 
 #pragma once
 
@@ -6,12 +6,15 @@
 #include "GameFramework/GameModeBase.h"
 #include "T5UIGameModeBase.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class TEAM5_PROJECT_API AT5UIGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	AT5UIGameModeBase(); // 생성자 추가
+
+	// [여기가 핵심!] 이 줄이 있어야 블루프린트에서 검색이 됩니다.
+	UFUNCTION(BlueprintCallable, Category = "GameFlow")
+	void StartGameSequence();
 };
